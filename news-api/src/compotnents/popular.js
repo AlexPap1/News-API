a42e6d5fa6dc9b65517ef0648c49614b38f9ce59
AlexPap <arpappagallo@gmail.com>
2022-11-03 18:33:31 -0400

    added gradient to background. clicking image in popular will open article in NEW TAB

--- news-api/src/compotnents/popular.js
+++ news-api/src/compotnents/popular.js
@@ -7,7 +7,7 @@ function loadPopular() {
             console.log(data);
             const popularContainer = document.getElementById("popularContainer");
             for(var i = 0; i < 15; i++) {
-                popularContainer.innerHTML += "<div> <h2 'font-weight = bold'> "+data.articles[i].title+" </h2> "+data.articles[i].description+" <br> <a href = "+data.articles[i].url+"> <img width = 300px; src="+data.articles[i].urlToImage+" </img> </a> </div>";
+                popularContainer.innerHTML += "<div> <h2 'font-weight = bold'> "+data.articles[i].title+" </h2> "+data.articles[i].description+" <br> <a target=_blank rel=noopener noreferrer href = "+data.articles[i].url+"> <img width = 300px; src="+data.articles[i].urlToImage+" </img> </a> </div>";
                 // document.getElementById("popularContainer").innerHTML += "<div style = 'padding-top: 20px;'><img style='float:left; width: 150px;' src='"+response.articles[i].urlToImage+"'><h1"+response.articles[i].title+"</h1>"+response.articles[i].source.name+"<br>"+response.articles[i].description+" <a href='"+response.articles[i].url+"'target = '_blank'>"+response.articles[i].url+"</a></div>";
             }
         })
