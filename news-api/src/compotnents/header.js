d048b062ed4e5baf0787972f7f892205fe4f76f4
AlexPap <arpappagallo@gmail.com>
2022-11-04 14:39:33 -0400

    can overwrite searches now and sortby publish date

--- news-api/src/compotnents/header.js
+++ news-api/src/compotnents/header.js
@@ -11,14 +11,48 @@ const Header = () => {
                     <button className="btn" type="submit" id="searchButton" onClick={searchResults}>
                         Search
                     </button>
+                    <button className="btn" type="submit" id="sortButton" onClick={sortResults}>
+                        Sort By New
+                    </button>
                 </div>
             </div>
         </header>
     )
 }
 
+function sortResults() {
+    const results = document.getElementById("results");
+    results.innerHTML = "";
+    document.getElementById("searchArea")
+    .addEventListener("keyup", function (e) {
+        if (e.keyCode === 13) {
+            document.getElementById("searchButton").click();
+        }
+    });
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '-' + dd + '-' + yyyy;
+    console.log(today);
+    var searchArea = document.getElementById("searchArea").value.replace(/\s/g, '_');
+    let api = `https://newsapi.org/v2/everything?q=` + searchArea + `&from=` + today + `&sortBy=publishedAt&apiKey=05f61c9469e447c89bbbd7f32ef76055`
+    console.log(api);
+    fetch(api).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            //displays15 articles
+            for(var i = 0; i < 15; i++) {
+                results.innerHTML += "<div style = 'border: 3px solid white; margin: 10px'> <h2 style = 'padding-left: 10px'> "+data.articles[i].title+" </h2> <p style = 'font-size: 20px; padding-left: 10px'> "+data.articles[i].description+" <p> <br> <a target=_blank rel=noopener noreferrer href = "+data.articles[i].url+"> <img style = 'width: 300px; padding-left: 10px' src="+data.articles[i].urlToImage+" </img> </a> </div>";
+            }
+        })
+    })
+}
+
 
 function searchResults() {
+    const results = document.getElementById("results");
+    results.innerHTML = "";
     document.getElementById("searchArea")
     .addEventListener("keyup", function (e) {
         if (e.keyCode === 13) {
@@ -37,7 +71,6 @@ function searchResults() {
     fetch(api).then((response) => {
         response.json().then((data) => {
             console.log(data);
-            const results = document.getElementById("results");
             //displays15 articles
             for(var i = 0; i < 15; i++) {
                 results.innerHTML += "<div style = 'border: 3px solid white; margin: 10px'> <h2 style = 'padding-left: 10px'> "+data.articles[i].title+" </h2> <p style = 'font-size: 20px; padding-left: 10px'> "+data.articles[i].description+" <p> <br> <a target=_blank rel=noopener noreferrer href = "+data.articles[i].url+"> <img style = 'width: 300px; padding-left: 10px' src="+data.articles[i].urlToImage+" </img> </a> </div>";
