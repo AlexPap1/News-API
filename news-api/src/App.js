942a0192f6e76eac6d0f9ead88c7aab2e69e5cf8
AlexPap <arpappagallo@gmail.com>
2022-11-04 12:07:54 -0400

    search is functional. unale to do double search/overwrite. needs sort as well

--- news-api/src/App.js
+++ news-api/src/App.js
@@ -13,22 +13,7 @@ function App() {
             <Popular />
           </div>
           <div classname = "search">
-            <div className="results">
-              <div id="search1">1</div>
-              <div id="search2">2</div>
-              <div id="search3">3</div>
-              <div id="search4">4</div>
-              <div id="search5">5</div>
-              <div id="search6">6</div>
-              <div id="search7">7</div>
-              <div id="search8">8</div>
-              <div id="search9">9</div>
-              <div id="search10">10</div>
-              <div id="search11">11</div>
-              <div id="search12">12</div>
-              <div id="search13">13</div>
-              <div id="search14">14</div>
-              <div id="search15">15</div>
+            <div className="results" id="results">
             </div>
           </div>
         </div>
--- news-api/src/compotnents/header.js
+++ news-api/src/compotnents/header.js
@@ -8,7 +8,7 @@ const Header = () => {
                 <h1>Your News Today!</h1>
                 <div className="search-area">
                     <input id="searchArea" className="search" placeholder="Search for News" size="30"></input>
-                    <button className="btn" type="button" id="searchButton" onClick={searchResults}>
+                    <button className="btn" type="submit" id="searchButton" onClick={searchResults}>
                         Search
                     </button>
                 </div>
@@ -19,6 +19,12 @@ const Header = () => {
 
 
 function searchResults() {
+    document.getElementById("searchArea")
+    .addEventListener("keyup", function (e) {
+        if (e.keyCode === 13) {
+            document.getElementById("searchButton").click();
+        }
+    });
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -26,10 +32,17 @@ function searchResults() {
     today = mm + '-' + dd + '-' + yyyy;
     console.log(today);
     var searchArea = document.getElementById("searchArea").value.replace(/\s/g, '_');
-    let api = `https://newsapi.org/v2/everything?q=` + searchArea + `&from=` + today + `&sortBy=popularity&apiKey=05f61c9469e447c89bbbd7f32ef76055`
+    let api = `https://newsapi.org/v2/everything?q=` + searchArea + `&from=` + today + `&sortBy=relevancy&apiKey=05f61c9469e447c89bbbd7f32ef76055`
     console.log(api);
-    fetch(api).then((res) => {
-        let searchResults = res;
+    fetch(api).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            const results = document.getElementById("results");
+            //displays15 articles
+            for(var i = 0; i < 15; i++) {
+                results.innerHTML += "<div style = 'border: 3px solid white; margin: 10px'> <h2 style = 'padding-left: 10px'> "+data.articles[i].title+" </h2> <p style = 'font-size: 20px; padding-left: 10px'> "+data.articles[i].description+" <p> <br> <a target=_blank rel=noopener noreferrer href = "+data.articles[i].url+"> <img style = 'width: 300px; padding-left: 10px' src="+data.articles[i].urlToImage+" </img> </a> </div>";
+            }
+        })
     })
 }
 
